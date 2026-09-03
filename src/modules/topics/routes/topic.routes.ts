2db67be774f1b114db87760c6a052d43f3c6498e
Pietro <piietro.pereira@hotmail.com>
2026-09-03 10:52:02 -0300

    feat: implement report content logic and dynamic flag button button on feed cards

--- src/modules/topics/routes/topic.routes.ts
+++ src/modules/topics/routes/topic.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import { TopicController } from '../controllers/TopicController';
 import { ensureAuthenticated } from '../../../middlewares/ensureAuthenticated';
+import { ReportController } from '../controllers/ReportController'; 
 
 const topicRoutes = Router();
 const topicController = new TopicController();
+const reportController = new ReportController();
 
 // Rotas
 topicRoutes.post('/', ensureAuthenticated, topicController.create); // Protegida por Token
 topicRoutes.get('/', topicController.list); // Pública para a comunidade ler
+topicRoutes.post('/report', ensureAuthenticated, reportController.create);
 
 export { topicRoutes };
